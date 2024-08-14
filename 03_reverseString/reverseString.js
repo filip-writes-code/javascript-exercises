e03baee7ee8464ce535afeb0609ed90d8d9f7166
Filip <tjofee47@gmail.com>
2024-08-14 16:32:47 -0600

    03: Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const characters = string.split("");
+    let reversedWord = "";
+    for (let i = characters.length - 1; i >= 0; i--) {
+        reversedWord += characters[i];
+    }
+    return reversedWord;
 };
 
+reverseString('Hola');
 // Do not edit below this line
 module.exports = reverseString;
