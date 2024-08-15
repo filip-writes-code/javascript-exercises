53d28fc5f7be99eb86a3558544e1e3cf2e364423
Filip <tjofee47@gmail.com>
2024-08-14 21:57:20 -0600

    05: Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+function compareNumbers(a, b)
+{
+    return a - b;
 };
+
+const sumAll = function(...inputs) {
+
+    for (const value of inputs) {
+        if ( !Number.isInteger(value) || value < 0 ) {
+            return "ERROR";
+        }
+    }
+
+    inputs.sort(compareNumbers);
+    let sum = 0;
+    for (let i = inputs[0]; i <= inputs[1]; i++ ) {
+        sum += i;
+    }
+    return sum;
+};
+
+
+//test stuff
+console.log(sumAll(124, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
