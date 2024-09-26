2e4daf7f2014dc573bd9ca2025a1af0aec54e762
Filip <tjofee47@gmail.com>
2024-09-26 16:54:14 -0600

    Added calculator logic

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbersArray) {
+	return numbersArray.reduce((sum, currentItem) => sum + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbersArray) {
+  return numbersArray.reduce((total, currentItem) => total * currentItem);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  if (input === 0) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = input; i > 0; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
