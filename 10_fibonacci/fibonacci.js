0941d8014ff2d239a52f0ce0ac1859969e00d1a1
Filip <tjofee47@gmail.com>
2024-09-26 18:29:50 -0600

    Fib sequence members

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member < 0) {return "OOPS"}; //exception for negative numbers
 
+    let fib = [0, 1]; //initialize the array
+    //generate the fib sequence
+    for (i=2; i <= member; i++) { 
+        fib[i] = fib[i-2] + fib[i-1];
+    };
+
+    return fib[member];
 };
 
+fibonacci(-4);
 // Do not edit below this line
 module.exports = fibonacci;
