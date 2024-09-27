ce93e31306217d89e951d012f973c5e612989b49
Filip <tjofee47@gmail.com>
2024-09-26 20:07:20 -0600

    Getting the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(booksArr) {
+    return booksArr.map(item => item.title);
 };
 
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
