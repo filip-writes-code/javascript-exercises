0c01dd5794a5febecb1c7269f466797003a7807b
Filip <tjofee47@gmail.com>
2024-09-26 18:02:40 -0600

    Add palindrome evaluation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
+    const word = input.split("") //splits characters into an array
+    .filter(character => {return /[a-zA-Z0-9]/.test(character)}) //strips of punctuation
+    .map(element => element.toLowerCase())   //converts to lowercase for comparison
+    .join(""); //and joins it back into a string
+
+    const reversedWord = word.split("").reverse().join("");
+
+    if (word === reversedWord) {return true } else {return false};
 };
 
+palindromes('ZZZZ car, a man, a maracaz.')
 // Do not edit below this line
 module.exports = palindromes;
