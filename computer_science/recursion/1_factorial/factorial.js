41b34601cc3c5c4e65434e5315dc0aedc94b2560
filip-writes-code <177264513+filip-writes-code@users.noreply.github.com>
2026-07-28 12:58:43 -0600

    Add Recursive Factorial function

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,14 @@
-const factorial = function() {
-
+const factorial = function (input) {
+  //if input conditions are not met, return undefined
+  if (!Number.isInteger(input) || input < 0) {
+    return undefined;
+  }
+  if (input === 0) {
+    // if base case, return 1
+    return 1;
+  }
+  //if not, go into recursion
+  return input * factorial(input - 1);
 };
 
 // Do not edit below this line
