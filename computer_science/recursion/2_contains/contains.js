f418778be6c730471aa9c49ba2078b8339dbc1f1
filip-writes-code <177264513+filip-writes-code@users.noreply.github.com>
2026-07-28 14:41:58 -0600

    Add Recursive function that searches nested objects for a value

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,17 @@
-const contains = function() {
-  
+const isAnObject = (val) =>
+  val !== null && typeof val === "object" && !Array.isArray(val);
+
+const contains = function (searchObject, searchValue) {
+  const values = Object.values(searchObject);
+  if (values.includes(searchValue)) return true;
+
+  const nestedObjects = values.filter((value) => isAnObject(value));
+  for (const object of nestedObjects) {
+    const recursiveSearch = contains(object, searchValue);
+    if (recursiveSearch) return true;
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
