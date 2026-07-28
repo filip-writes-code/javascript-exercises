57e70e6fbdcc11f967a515c63647163bbcfd2970
filip-writes-code <177264513+filip-writes-code@users.noreply.github.com>
2026-07-28 16:13:45 -0600

    Add totalInteger() function that counts integers in deeply nested arrays and objects

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,19 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (searchObject) {
+  let total = 0;
+
+  if (typeof searchObject !== "object") return;
+
+  const values = Object.values(searchObject);
+  for (const value of values) {
+    if (Number.isInteger(value)) {
+      total++;
+    }
+    if (typeof value === "object" && value !== null)
+      total += totalIntegers(value);
+  }
+
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
