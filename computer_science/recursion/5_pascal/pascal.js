67cc11a2ae770f1dd45de595b7af5d2a879e9685
filip-writes-code <177264513+filip-writes-code@users.noreply.github.com>
2026-07-29 15:33:43 -0600

    Add Recursive Pascal Triangle Function

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,17 @@
-const pascal = function() {
-  
+const pascal = function (n, result = []) {
+  if (n === 1) return [1];
+
+  let previousRow = pascal(n - 1);
+  previousRow.push(0);
+  previousRow.unshift(0);
+
+  for (let i = 0; i < previousRow.length - 1; i++) {
+    const cell = previousRow[i] + previousRow[i + 1];
+    result.push(cell);
+  }
+
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
