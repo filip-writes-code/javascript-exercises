aef695dc53449829694270cc0edfc9dc3189083c
filip-writes-code <177264513+filip-writes-code@users.noreply.github.com>
2026-07-29 13:22:38 -0600

    Add Recursive function for permutating numbers in arrays

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,22 @@
-const permutations = function() {
-  
+const permutations = function (nums) {
+  let result = [];
+
+  if (nums.length === 0) return [[]];
+  if (nums.length === 1) return [nums];
+
+  for (let i = 0; i < nums.length; i++) {
+    let currentNumber = nums[i];
+    let remainingNumbers = nums.toSpliced(i, 1);
+    let remainingNumsPermutated = permutations(remainingNumbers);
+
+    for (let j = 0; j < remainingNumsPermutated.length; j++) {
+      let permutatedArray = [currentNumber].concat(remainingNumsPermutated[j]);
+      result.push(permutatedArray);
+    }
+  }
+
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
